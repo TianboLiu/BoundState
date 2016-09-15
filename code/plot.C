330eac34d0a1da194ed4a5176cb176fb686b89ca
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2016-09-15 08:28:55 -0400

    Add new ds file

--- code/plot.C
+++ code/plot.C
@@ -12,24 +12,12 @@ using namespace std;
 
 int main(){
 
-  LoadDS("ds1450.dat");
-  cout << sigmatotal() << endl;
+  LoadDS("ds.dat");
+  cout << sigmatotal()*3.89379e5*132.0 << endl;
   
   if (false){
-    double x, y, z;
-    ifstream infile("ds1450.dat");
-    
-    TGraph2D * g2 = new TGraph2D(2800);
-    
-    for (int i = 0; i < 2800; i++){
-    infile >> x >> y >> z;
-    if (z < 1.0e-60) z = 1.0e-60;
-    g2->SetPoint(i, x, y, z);
-    }
-    infile.close();
-    
     TCanvas * c1 = new TCanvas("", "", 800, 600);
-    g2->Draw("surf1");
+    ds2D.Draw("surf1");
     
     c1->Print("c1.pdf");
   }
