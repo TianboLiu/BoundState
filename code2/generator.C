d763566d50c4ea2f36ea2a285aefde6142c769c7
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2016-09-30 16:50:40 -0400

    Update .C files

--- code2/generator.C
+++ code2/generator.C
@@ -3,9 +3,19 @@
 using namespace std;
 
 int main(int argc, char * argv[]){
+  bool Prepare = true;
+  if (Prepare){
+    //Get wave function and potential grid, and setup interpolation
+    readgrid("wf.dat", "Veff.dat");
+    //Wave function renormalization
+    urone();
+    //cout << urone() << endl;
+    //Generate or load FQ grid
+    //makeFQ(fFQ);
+    LoadFQ("FQ.dat");
+  }
 
-  LoadDS(argv[1]);
-  genData(argv[2], 10000000);//ds grid file, create root file, Nsim
+  genData(argv[1], 10000000);//create root file, Nsim
 
   return 0;
 }
--- code2/plot.C
+++ code2/plot.C
@@ -13,16 +13,16 @@ using namespace std;
 int main(){
   if (false){
     LoadDS("DS/ds1450ss.dat");
-    double ss = sigmatotal()*3.89379e5;
+    double ss = sigmaT_inter()*3.89379e5;
     cout << ss << endl;
     LoadDS("DS/ds1450sp.dat");
-    double sp = sigmatotal()*3.89379e5;
+    double sp = sigmaT_inter()*3.89379e5;
     cout << sp << endl;
     LoadDS("DS/ds1450ps.dat");
-    double ps = sigmatotal()*3.89379e5;
+    double ps = sigmaT_inter()*3.89379e5;
     cout << ps << endl;
     LoadDS("DS/ds1450pp.dat");
-    double pp = sigmatotal()*3.89379e5;
+    double pp = sigmaT_inter()*3.89379e5;
     cout << pp << endl;
     cout << 12*ss + 32*sp + 32*ps + 56*pp << endl;
   }
@@ -31,7 +31,7 @@ int main(){
 
   if (true){
     LoadDS("DS/ds1450t.dat");
-    cout << sigmatotal()*3.89379e5 << endl;
+    cout << sigmaT_inter()*3.89379e5 << endl;
     TCanvas * c1 = new TCanvas("ds", "ds", 800, 600);
     ds2D.Draw("surf1");
     
