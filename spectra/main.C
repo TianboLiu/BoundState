a4f5521192def596b0280a1ef61440e5d74a7714
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2016-10-11 20:12:41 -0400

    spectral fit .C code

--- spectra/main.C
+++ spectra/main.C
@@ -2,8 +2,21 @@
 
 using namespace std;
 
-int main(){
-  //makeset();
-  fits();
+int main(int argc, char * argv[]){
+  char * control = argv[1];
+  if (strcmp("set", control) == 0){
+    makeset();
+  }
+  if (strcmp("read", control) == 0){
+    readset(argv[2]);
+  }
+  if (strcmp("fit", control) == 0){
+    readset(argv[2]);
+    FitMissingMomentum();
+  }
+  if (strcmp("energy", control) == 0){
+    readset(argv[2]);
+    FitMissingEnergy();
+  }
   return 0;
 }
