3c14ff7caff72adedbe4f80ff59dc8a76106428f
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2016-10-17 23:23:55 -0400

    update fit code

--- fit/main.C
+++ fit/main.C
@@ -3,18 +3,63 @@
 using namespace std;
 
 int main(int argc, char * argv[]){
-  //double s = atof(argv[1]);
+  //double fixE = atof(argv[1]);
   double s = 1.985;
-  for (int i = 0; i < 100; i++){
-    s = 1.985 + 0.01 * i;
-    creatset("dataset.dat", "clasdb_E63M1.txt", s);
-    addset("dataset.dat", "clasdb_E63M2.txt", s);
-    f0Fit();
-    if (s > 2.175) break;
+  //double s = fixE;
+  double par[5];
+  if (false){
+    double Chi2 = 0.0;
+    FILE * fp = fopen("fitphi.txt", "w");
+    fprintf(fp, "sqrt(s)\t a0\t a2\t b1\t\n");
+    for (int i = 0; i < 100; i++){
+      s = 1.985 + 0.01 * i;
+      creatset("dataset.dat", "clasdb_E63M1.txt", s);
+      addset("dataset.dat", "clasdb_E63M2.txt", s);
+      Chi2 += f0Fit(par);
+      //plotdata("dataset.dat", par);
+      fprintf(fp, "%.3f\t %.4f\t %.4f\t %.4f\n", s, par[0], par[1], par[2]);
+      if (s >= 2.835) break;
+    }
+    fclose(fp);
+    cout << "Chi2 = " << Chi2 << endl;
+  }
+ 
+  if (false){
+    ifstream infile("fitphi.txt");
+    char tmp[256];
+    infile.getline(tmp, 256);
+    TString filename;
+    while (infile >> par[0] >> par[1] >> par[2] >> par[3]){
+      filename = Form("gallery/ds_phi_%.4d.pdf", (int) (par[0] * 1000));
+      printplot(filename.Data(), par);
+      //break;
+    }
+    infile.close();
   }
 
-  //plotdata("dataset.dat");
-  //f0Fit();
+  if (true){
+    double globalpar[5] = {0.224551, 2.00386, 3.75764, 1.38537, 0.909071};
+    sigma2.SetParameters(globalpar);
+    TString filename;
+    double ss = 1.985;
+    while (ss <= 2.835){
+      filename = Form("gallery/global_ds_phi_%.4d.pdf", (int) (ss * 1000 + 1.0e-3));
+      printplot2(filename.Data(), &ss);
+      ss += 0.01;
+    }
+  }
+
+  if (false){
+    double Chi2 = 0.0;
+    creatset("dataset.dat", "clasdb_E63M1.txt", 1.9, 2.9);
+    addset("dataset.dat", "clasdb_E63M2.txt", 1.9, 2.9);
+    Chi2 = f1Fit(par);
+    cout << Chi2 << endl;
+  }
+    
+  if (false){
+    parameterplot("fitphi.txt");
+  }
 
   return 0;
 }
--- fit/phifit.h
+++ fit/phifit.h
@@ -12,9 +12,38 @@
 #include "Math/Factory.h"
 #include "Math/Functor.h"
 #include "TF1.h"
+#include "TF2.h"
 #include "TH1D.h"
 #include "TGraphErrors.h"
 #include "TCanvas.h"
+
+double dsigma(const double * var, const double * par){
+  double x = var[0];
+  double r0 = exp(par[2] * x) * par[2] / (2.0 * sinh(par[2]));
+  double r2 = x * x * exp(par[2] * x) * pow(par[2], 3) / (2.0 * (par[2] * par[2] + 2.0) * sinh(par[2]) - 4.0 * par[2] * cosh(par[2]));
+  double result = par[0] * (r0 + par[1] * r2) / (1.0 + par[1]);
+  return result;
+}
+
+double ds_global(const double * var, const double * par){
+  double sr = var[0];//sqrt(s)
+  double x = var[1];//cos theta
+  double M = 1.957727;
+  double b1 = par[3] * pow(sr * sr - M * M, par[4]);
+  double a2 = par[2] * (sr - M);
+  double a0 = par[0] * atan(par[1] * par[1]* (sr * sr - M * M));
+  double r0 = exp(b1 * x) * b1 / (2.0 * sinh(b1));
+  double r2 = x * x * exp(b1 * x) * pow(b1, 3) / (2.0 * (b1 * b1 + 2.0) * sinh(b1) - 4.0 * b1 * cosh(b1));
+  double result = a0 * (r0 + a2 * r2) / (1.0 + a2);
+  return result;
+}
+  
+TF1 sigma0("sigma0", "[0]", -1, 1);
+TF1 sigma("sigma", "[0]*exp(-[1]*(1-x)-[2]*(1-x)*(1-x))", -1, 1);
+TF1 sigma3("sigma3", "[0]+x*[1]*exp(-[2]*(1.0-x))", -1, 1);
+TF1 sigma1("sigma1", dsigma, -1, 1, 3);
+TF2 sigma2("sigma1", ds_global, 1.96, 3.0, -1.0, 1.0, 5);
+
 
 int creatset(const char * savefile, const char * datafile, const double Emin, const double Emax = 0){
   double Erange[2] = {Emin, Emax};
@@ -70,7 +99,7 @@ int addset(const char * savefile, const char * datafile, const double Emin, cons
   return 0;
 }
 
-int plotdata(const char * datafile){
+int plotdata(const char * datafile, const double * par){
   int Nt = 0;
   double Es, cth, ds, err;
   std::ifstream infile(datafile);
@@ -96,64 +125,238 @@ int plotdata(const char * datafile){
   h0->GetYaxis()->SetTitle("d#sigma / dcos#theta_{c.m.} (#mub)");
   h0->GetYaxis()->CenterTitle();
   h0->GetYaxis()->SetTitleOffset(1.5);
-  TF1 f0("f0", "[0]", -1, 1); 
-  f0.SetParameter(0, (lower+upper)/2.0);
-  g0->Fit(&f0);
+  sigma1.SetParameters(par);
+  sigma1.SetLineWidth(1);
+  sigma1.SetLineColor(2);
+  //g0->Fit(&f0);
   g0->SetMarkerStyle(8);
   g0->SetMarkerSize(0.6);
   g0->SetMarkerColor(4);
   g0->SetLineWidth(0.5);
-  g0->GetFunction("f0")->SetLineWidth(1);
-  g0->GetFunction("f0")->SetLineColor(2);
+  //g0->GetFunction("f0")->SetLineWidth(1);
+  //g0->GetFunction("f0")->SetLineColor(2);
   TCanvas * c0 = new TCanvas("", "", 800, 600);
   gStyle->SetOptStat(0);
   h0->Draw();
   g0->Draw("pesame");
+  sigma1.Draw("same");
   c0->Print("plotdata.pdf");
   return 0;
 }
 
-TF1 sigma0("sigma0", "[0]", -1, 1);
-TF1 sigma("sigma", "[0]*exp(-[1]*(1-x)-[2]*(1-x)*(1-x))", -1, 1);
-TF1 sigma2("sigma2", "[0]*(1+[1]*x+[2]*x*(x + abs(x)))", -1, 1);
-TF1 sigma3("sigma3", "[0]+x*[1]*exp(-[2]*(1.0-x))", -1, 1);
-TF1 sigma1("sigma1", "([0] + [1] * x) * exp([2] * x)", -1, 1);
+int printplot(const char * savefile, const double * par){
+  double s = par[0];
+  creatset("plotset.dat", "clasdb_E63M1.txt", s);
+  addset("plotset.dat", "clasdb_E63M2.txt", s);
+  int Nt = 0;
+  double Es, cth, ds, err;
+  std::ifstream infile("plotset.dat");
+  while (infile >> Es >> cth >> ds >> err) Nt++;//count data points
+  std::cout << Nt << std::endl;
+  infile.clear();
+  infile.seekg(0, std::ios::beg);//go back to the beginning of the datafile
+  TGraphErrors * g0 = new TGraphErrors(Nt);
+  double upper, lower;
+  for (int i = 0; i < Nt; i++){
+    infile >> Es >> cth >> ds >> err;
+    g0->SetPoint(i, cth, ds);
+    g0->SetPointError(i, 0, err);
+    if (i == 0) {upper = ds + err; lower = ds - err;}
+    if (upper < ds + err) upper = ds + err;
+    if (lower > ds - err) lower = ds - err;
+  }
+  infile.close();
+  TString title = Form("#sqrt{s} = %.3f GeV", s);
+  TH1D * h0 = new TH1D("h0", title, 1, -1.0, 1.0);
+  h0->Fill(0.0, -100.0);
+  h0->GetYaxis()->SetRangeUser(-0.02, lower+upper);
+  h0->GetXaxis()->SetTitle("cos#theta_{c.m.}");
+  h0->GetXaxis()->CenterTitle();
+  h0->GetYaxis()->SetTitle("d#sigma / dcos#theta_{c.m.} (#mub)");
+  h0->GetYaxis()->CenterTitle();
+  h0->GetYaxis()->SetTitleOffset(1.5);
+  sigma1.SetParameters(&par[1]);
+  sigma1.SetLineWidth(1);
+  sigma1.SetLineColor(2);
+  g0->SetMarkerStyle(8);
+  g0->SetMarkerSize(0.6);
+  g0->SetMarkerColor(4);
+  g0->SetLineWidth(0.5);
+  TCanvas * c0 = new TCanvas("", "", 800, 600);
+  gStyle->SetOptStat(0);
+  h0->Draw();
+  g0->Draw("pesame");
+  sigma1.Draw("same");
+  c0->Print(savefile);
+  h0->Delete();
+  g0->Delete();
+  c0->Close();
+  return 0;
+}
 
-
-
+int printplot2(const char * savefile, const double * par){
+  double s = par[0];
+  creatset("plotset.dat", "clasdb_E63M1.txt", s);
+  addset("plotset.dat", "clasdb_E63M2.txt", s);
+  int Nt = 0;
+  double Es, cth, ds, err;
+  std::ifstream infile("plotset.dat");
+  while (infile >> Es >> cth >> ds >> err) Nt++;//count data points
+  std::cout << Nt << std::endl;
+  infile.clear();
+  infile.seekg(0, std::ios::beg);//go back to the beginning of the datafile
+  TGraphErrors * g0 = new TGraphErrors(Nt);
+  double upper, lower;
+  for (int i = 0; i < Nt; i++){
+    infile >> Es >> cth >> ds >> err;
+    g0->SetPoint(i, cth, ds);
+    g0->SetPointError(i, 0, err);
+    if (i == 0) {upper = ds + err; lower = ds - err;}
+    if (upper < ds + err) upper = ds + err;
+    if (lower > ds - err) lower = ds - err;
+  }
+  infile.close();
+  TString title = Form("#sqrt{s} = %.3f GeV", s);
+  TH1D * h0 = new TH1D("h0", title, 1, -1.0, 1.0);
+  h0->Fill(0.0, -100.0);
+  h0->GetYaxis()->SetRangeUser(-0.02, lower+upper);
+  h0->GetXaxis()->SetTitle("cos#theta_{c.m.}");
+  h0->GetXaxis()->CenterTitle();
+  h0->GetYaxis()->SetTitle("d#sigma / dcos#theta_{c.m.} (#mub)");
+  h0->GetYaxis()->CenterTitle();
+  h0->GetYaxis()->SetTitleOffset(1.5);
+  TGraph * g1 = new TGraph(101);
+  for (int i = 0; i < 101; i++){
+    g1->SetPoint(i, -1.0+0.02*i, sigma2.Eval(Es, -1.0+0.02*i));
+    //std::cout << Es << " " << -1.0+0.02*i << " " << sigma2.Eval(Es, -1.0+0.02*i) << std::endl;
+  }
+  g1->SetLineColor(2);
+  g1->SetLineWidth(2);
+  g0->SetMarkerStyle(8);
+  g0->SetMarkerSize(0.6);
+  g0->SetMarkerColor(4);
+  TCanvas * c0 = new TCanvas("", "", 800, 600);
+  gStyle->SetOptStat(0);
+  h0->Draw();
+  g0->Draw("pesame");
+  g1->Draw("csame");
+  c0->Print(savefile);
+  h0->Delete();
+  g0->Delete();
+  g1->Delete();
+  c0->Close();
+  return 0;
+}
 
 double f0(const double * par){
   std::ifstream infile("dataset.dat");
   double sum = 0.0;
   sigma1.SetParameters(par);
-  double Es, cth, ds, err;
+  double Es, cth, ds, err, err2;
   while (!infile.eof()){
     infile >> Es >> cth >> ds >> err;
-    sum += pow(sigma1.Eval(cth) - ds, 2) / (err * err);
+    err2 = err*err + 0.11*0.11*ds*ds;
+    sum += pow(sigma1.Eval(cth) - ds, 2) / err2;
+  }
+  infile.close();
+  return sum;
+}
+
+double f1(const double * par){
+  std::ifstream infile("dataset.dat");
+  double sum = 0.0;
+  sigma2.SetParameters(par);
+  double Es, cth, ds, err, err2;
+  while (!infile.eof()){
+    infile >> Es >> cth >> ds >> err;
+    err2 = err*err + 0.11*0.11*ds*ds;
+    sum += pow(sigma2.Eval(Es, cth) - ds, 2) / err2;
   }
   infile.close();
   return sum;
 }
 
 
-int f0Fit(const char * minName = "Minuit2", const char * algoName = "Migrad"){
+double f0Fit(double * par, const char * minName = "Minuit2", const char * algoName = "Migrad"){
+  const int NPAR = 3;
   ROOT::Math::Minimizer * min = ROOT::Math::Factory::CreateMinimizer(minName, algoName);
   min->SetMaxFunctionCalls(100000);
   min->SetTolerance(1.0e-6);
   min->SetPrintLevel(0);
-  ROOT::Math::Functor f(&f0, 3);
+  ROOT::Math::Functor f(&f0, NPAR);
   min->SetFunction(f);
   min->SetVariable(0, "A", 0.1, 1.0e-3);
-  min->SetVariable(1, "B", 0.0, 1.0e-3);
-  min->SetVariable(2, "C", 0.0, 1.0e-3);
+  //min->SetVariable(1, "b", 0.0, 1.0e-3);
+  min->SetLimitedVariable(1, "B", 0.0, 1.0e-3, 0.0, 3.0);
+  min->SetVariable(2, "C", 0.05, 1.0e-3);
   min->Minimize();
   const double * xs = min->X();
+  for (int i = 0; i < NPAR; i++)
+    par[i] = xs[i];
   const double chi2 = min->MinValue();
   sigma1.SetParameters(xs);
   double total = sigma1.Integral(-1,1);
   std::cout << total << " " << chi2 << " " << xs[0] << "   " << xs[1] << "   " << xs[2] << std::endl;
+  return chi2;
+}
+
+double f1Fit(double * par, const char * minName = "Minuit2", const char * algoName = "Migrad"){
+  const int NPAR = 5;
+  ROOT::Math::Minimizer * min = ROOT::Math::Factory::CreateMinimizer(minName, algoName);
+  min->SetMaxFunctionCalls(100000);
+  min->SetTolerance(1.0e-6);
+  min->SetPrintLevel(0);
+  ROOT::Math::Functor f(&f1, NPAR);
+  min->SetFunction(f);
+  min->SetVariable(0, "a0", 0.2267, 1.0e-4);
+  min->SetVariable(1, "a1", 2.06, 1.0e-4);
+  min->SetVariable(2, "a2", 3.75, 1.0e-4);
+  min->SetVariable(3, "b1", 1.38, 1.0e-4);
+  min->SetVariable(4, "b2", 0.91, 1.0e-4);
+  min->Minimize();
+  const double * xs = min->X();
+  for (int i = 0; i < NPAR; i++)
+    par[i] = xs[i];
+  const double chi2 = min->MinValue();
+  sigma1.SetParameters(xs);
+  double total = sigma1.Integral(-1,1);
+  std::cout << total << " " << chi2 << " " << xs[0] << "   " << xs[1] << "   " << xs[2] << "   " << xs[3] << "   " << xs[4] << std::endl;
+  return chi2;
+}
+
+int parameterplot(const char * parset){
+  double par[4];
+  int Np = -1;
+  std::ifstream infile(parset);
+  char tmp[256];
+  while(infile.getline(tmp, 256)){
+    Np++;
+  }
+  infile.clear();
+  infile.seekg(0, std::ios::beg);
+  TH1D * h0 = new TH1D("h0", "", 1, 1.9, 2.9);
+  h0->GetYaxis()->SetRangeUser(0.0, 6);
+  TGraph * g0 = new TGraph(Np);
+  infile.getline(tmp, 256);
+  for (int i = 0; i < Np - 1; i++){
+    infile >> par[0] >> par[1] >> par[2] >> par[3];
+    std::cout << par[0] << " " << par[3] << std::endl;
+    g0->SetPoint(i, par[0], par[3]);
+  }
+  g0->SetMarkerStyle(8);
+  g0->SetMarkerSize(0.6);
+  g0->SetMarkerColor(4);
+  gStyle->SetOptStat(0);
+  TCanvas * c0 = new TCanvas("c0", "", 800, 600);
+  h0->Draw();
+  g0->Draw("psame");
+  c0->Print("parplot.pdf");
+  h0->Delete();
+  g0->Delete();
+  c0->Close();
   return 0;
 }
+
 
 
 
