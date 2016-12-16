7b6c95a8883ad3af4b7deb2561146b2904241d8a
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2016-12-16 02:46:18 -0500

    update plot

--- code2/plotter.C
+++ code2/plotter.C
@@ -18,19 +18,22 @@ int main(){
       gwf->SetPoint(i, 0.05*i, u);
     }
     TCanvas * cwf = new TCanvas("cwf", "u(r)", 800, 600);
+    cwf->SetLeftMargin(0.15);
+    cwf->SetBottomMargin(0.15);
+    
     gwf->SetTitle("wave function");
     gwf->SetLineColor(4);
-    gwf->SetLineWidth(2);
-    gwf->GetXaxis()->SetTitle("r/fm");
-    gwf->GetXaxis()->SetTitleSize(0.05);
-    gwf->GetXaxis()->SetLabelSize(0.05);
-    gwf->GetXaxis()->CenterTitle();
+    gwf->SetLineWidth(2.5);
+    gwf->GetXaxis()->SetTitle("r / fm");
+    gwf->GetXaxis()->SetTitleSize(0.06);
+    gwf->GetXaxis()->SetLabelSize(0.055);
+    gwf->GetXaxis()->CenterTitle(true);
     gwf->GetXaxis()->SetRangeUser(0.0, 5.0);
-    gwf->GetYaxis()->SetTitle("u(r)/GeV^{1/2}");
-    gwf->GetYaxis()->SetTitleSize(0.05);
-    gwf->GetYaxis()->SetTitleOffset(1.0);
-    gwf->GetYaxis()->SetLabelSize(0.05);
-    gwf->GetYaxis()->CenterTitle();
+    gwf->GetYaxis()->SetTitle("u(r) / GeV^{1/2}");
+    gwf->GetYaxis()->SetTitleSize(0.06);
+    gwf->GetYaxis()->SetTitleOffset(1.15);
+    gwf->GetYaxis()->SetLabelSize(0.055);
+    gwf->GetYaxis()->CenterTitle(true);
     gwf->GetYaxis()->SetRangeUser(0.0, 0.5);
     gwf->GetYaxis()->SetNdivisions(5, 5, 0, kTRUE);
     gwf->Draw("AC");
@@ -46,17 +49,19 @@ int main(){
     }
     TCanvas * cV = new TCanvas("V", "V(r)", 800, 600);
     gV->SetTitle("effective potential");
+    
     gV->SetLineColor(4);
-    gV->SetLineWidth(2);
-    gV->GetXaxis()->SetTitle("r/fm");
-    gV->GetXaxis()->SetTitleSize(0.05);
-    gV->GetXaxis()->SetLabelSize(0.05);
+    gV->SetLineWidth(2.5);
+    gV->GetXaxis()->SetTitle("r / fm");
+    gV->GetXaxis()->SetTitleSize(0.06);
+    gV->GetXaxis()->SetLabelSize(0.055);
     gV->GetXaxis()->CenterTitle();
+    gV->GetXaxis()->SetTitleOffset(1.15);
     gV->GetXaxis()->SetRangeUser(0.0, 5.0);
     gV->GetYaxis()->SetTitle("V_{eff }/GeV");
-    gV->GetYaxis()->SetTitleSize(0.05);
-    gV->GetYaxis()->SetTitleOffset(1.0);
-    gV->GetYaxis()->SetLabelSize(0.05);
+    gV->GetYaxis()->SetTitleSize(0.06);
+    gV->GetYaxis()->SetTitleOffset(1.15);
+    gV->GetYaxis()->SetLabelSize(0.055);
     gV->GetYaxis()->CenterTitle();
     gV->GetYaxis()->SetRangeUser(-0.6, 0.2);
     gV->GetYaxis()->SetNdivisions(4, 5, 0, kTRUE);
