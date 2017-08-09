21cefc86f9fa9e1e4981beeb0dbc125dd78366ed
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2017-08-09 00:51:04 -0400

    update

--- code4/bump_detected.C
+++ code4/bump_detected.C
@@ -11,7 +11,7 @@ int main(const int argc, const char * argv[]){
   TLorentzVector ki[2], kf[5];
   double weight = 0;
   //ki[0].SetXYZT(0, 0, sqrt(4.4 * 4.4 - PARTICLE::e.M() * PARTICLE::e.M()), 4.4);
-  ki[0].SetXYZT(0, 0, 4.4, 4.4);
+  ki[0].SetXYZM(0, 0, 4.4, PARTICLE::e.M());
   
 
   TFile * fs = new TFile("result/bumpdetected.root", "RECREATE");
@@ -26,27 +26,34 @@ int main(const int argc, const char * argv[]){
   TH1D * H2 = new TH1D("CosThetaPhi_Gold", "", 1000, -1.0, 1.0);
   TH1D * H3 = new TH1D("E_g_Gold", "", 1000, 0.0, 4.4);
 
-  TH2D * s0 = new TH2D("ECosTheta", "", 200, 0.0, 4.4, 200, -1.0, 1.0);
+  TH2D * s0 = new TH2D("ECosTheta", "", 100, 1.9, 2.9, 200, -1.0, 1.0);
+  TH2D * s1 = new TH2D("ECosTheta_clas12", "", 100, 1.9, 2.9, 200, -1.0, 1.0);
+  TH2D * s2 = new TH2D("ECosTheta_bonus12", "", 100, 1.9, 2.9, 200, -1.0, 1.0);
   
-  TH2D * S0 = new TH2D("ECosTheta_Gold", "", 200, 0.0, 4.4, 200, -1.0, 1.0);
+  TH2D * S0 = new TH2D("ECosTheta_Gold", "", 100, 1.9, 2.9, 200, -1.0, 1.0);
+  TH2D * S1 = new TH2D("ECosTheta_Gold_clas12", "", 100, 1.9, 2.9, 200, -1.0, 1.0);
+  TH2D * S2 = new TH2D("ECosTheta_Gold_bonus12", "", 100, 1.9, 2.9, 200, -1.0, 1.0);
 
   TLorentzVector P2(0, 0, 0, Mp);//rest nucleon
   TLorentzVector P1, P3, P4;
   TLorentzVector Ptotal;
-  //double p1cm, p3cm, t, t0;
-  //double Ecm;
+  double acc = 0.0;
   for (Long64_t i = 0; i < Nsim; i++){
     if (i%1000000 == 0) cout << i << endl;
  
-    weight = GENERATE::Event_eNKK_Phi_Nucleon(ki, kf);
+    //weight = GENERATE::Event_eNKK_Phi_Nucleon(ki, kf);
+    weight = GENERATE::Event_eNKK_Phi_Nucleon(ki, kf) / 0.489;
+    
     if (weight > 0){
-      weight *= DETECTOR::Acceptance(kf[2], "K+") * DETECTOR::Acceptance(kf[3], "K-");
-      if (weight > 0){
+      //acc = DETECTOR::Acceptance(kf[2], "K+") * DETECTOR::Acceptance(kf[3], "K-");
+      acc = DETECTOR::AcceptancePhi(kf[2] + kf[3], "all");
+      if (acc > 0){
 	P1 = ki[0] - kf[0];//virtual photon
+	P2.SetXYZT(0, 0, 0, Mp);//rest proton
 	P3 = kf[2] + kf[3];//phi
 	P4 = kf[1];//N'
-	P2.SetXYZT(0, 0, 0, Mp);
-	Ptotal = kf[1] + kf[2] + kf[3];
+
+	Ptotal = P3 + P4;
 	
 	P1.Boost(-Ptotal.BoostVector());
 	P2.Boost(-Ptotal.BoostVector());
@@ -54,36 +61,52 @@ int main(const int argc, const char * argv[]){
 	P4.Boost(-Ptotal.BoostVector());
 	
 	
-	h0->Fill(P3.M(), weight);
-	h1->Fill(Ptotal.M(), weight);
-	h2->Fill(cos(P3.Angle(P1.Vect())), weight);
-	h3->Fill(P1.E(), weight);
+	h0->Fill(P3.M(), weight * acc);
+	h1->Fill(Ptotal.M(), weight * acc);
+	h2->Fill(cos(P3.Angle(P1.Vect())), weight * acc);
+	h3->Fill(P1.E(), weight * acc);
 	
-	s0->Fill(P1.E(), cos(P3.Angle(P1.Vect())), weight);
+	s0->Fill(Ptotal.M(), cos(P3.Angle(P1.Vect())), weight * acc);
+
+	//acc = DETECTOR::AcceptanceCLAS12(kf[2], "K+") * DETECTOR::AcceptanceCLAS12(kf[3], "K-");
+	acc = DETECTOR::AcceptancePhi(kf[2] + kf[3], "clas12");
+	s1->Fill(Ptotal.M(), cos(P3.Angle(P1.Vect())), weight * acc);
+
+	//acc = DETECTOR::AcceptanceBONUS12(kf[2], "K+") * DETECTOR::AcceptanceBONUS12(kf[3], "K-");
+	acc = DETECTOR::AcceptancePhi(kf[2] + kf[3], "bonus12");
+	s2->Fill(Ptotal.M(), cos(P3.Angle(P1.Vect())), weight * acc);	
+
       }
     }
 
     weight = GENERATE::Event_eNKK_Phi(ki, kf);
     if (weight > 0){
-      weight *= DETECTOR::Acceptance(kf[1], "p") * DETECTOR::Acceptance(kf[2], "K+") * DETECTOR::Acceptance(kf[3], "K-");
-      if (weight > 0){
+      weight = weight / 0.489;
+      acc = DETECTOR::AcceptancePhi(kf[2] + kf[3], "all") * DETECTOR::Acceptance(kf[1], "p");
+      if (acc > 0){
 	P1 = ki[0] - kf[0];//virtual photon
 	P3 = kf[2] + kf[3];//phi
 	P4 = kf[1];//N'
 	P2 = P3 + P4 - P1;
-	Ptotal = kf[1] + kf[2] + kf[3];
+	Ptotal = P3 + P4;
 	
 	P1.Boost(-Ptotal.BoostVector());
 	P2.Boost(-Ptotal.BoostVector());
 	P3.Boost(-Ptotal.BoostVector());
 	P4.Boost(-Ptotal.BoostVector());
 	
-	H0->Fill(P3.M(), weight);
-	H1->Fill(Ptotal.M(), weight);
-	H2->Fill(cos(P3.Angle(P1.Vect())), weight);//|t-t0|
-	H3->Fill(P1.E(), weight);
+	H0->Fill(P3.M(), weight * acc);
+	H1->Fill(Ptotal.M(), weight * acc);
+	H2->Fill(cos(P3.Angle(P1.Vect())), weight * acc);
+	H3->Fill(P1.E(), weight * acc);
 	
-	S0->Fill(P1.E(), cos(P3.Angle(P1.Vect())), weight);
+	S0->Fill(Ptotal.M(), cos(P3.Angle(P1.Vect())), weight * acc);
+
+	acc = DETECTOR::AcceptancePhi(kf[1], "clas12") * DETECTOR::AcceptanceCLAS12(kf[2], "p");
+	S1->Fill(Ptotal.M(), cos(P3.Angle(P1.Vect())), weight * acc);
+
+	acc = DETECTOR::AcceptancePhi(kf[1], "bonus12") * DETECTOR::AcceptanceBONUS12(kf[2], "p");
+	S2->Fill(Ptotal.M(), cos(P3.Angle(P1.Vect())), weight * acc);
       }
     }
   }
@@ -94,6 +117,8 @@ int main(const int argc, const char * argv[]){
   h3->Scale(1.0/Nsim);
 
   s0->Scale(1.0/Nsim);
+  s1->Scale(1.0/Nsim);
+  s2->Scale(1.0/Nsim);
  
   H0->Scale(1.0/Nsim);
   H1->Scale(1.0/Nsim);
@@ -101,7 +126,9 @@ int main(const int argc, const char * argv[]){
   H3->Scale(1.0/Nsim);
 
   S0->Scale(1.0/Nsim);
- 
+  S1->Scale(1.0/Nsim);
+  S2->Scale(1.0/Nsim);
+  
   fs->Write();
 
   return 0;
--- code4/plotter_bump.C
+++ code4/plotter_bump.C
@@ -1,10 +1,13 @@
 #include "Lcore.h"
+#include "TLatex.h"
 
 int main(const int argc, const char * argv[]){
   if (argc < 2) return 1;
 
   const int opt = atoi(argv[1]);
   gStyle->SetPalette(55);
+
+  cout << opt << endl;
 
   if (opt == 1){//total Egamma cm distribution
     TFile * fs = new TFile("result/bumpdetected.root", "r");
@@ -53,27 +56,30 @@ int main(const int argc, const char * argv[]){
   if (opt == 2){//total Egamma cm distribution
     TFile * fs = new TFile("result/bumpdetected.root", "r");
     TH2D * h2 = (TH2D *) fs->Get("ECosTheta");
-    TH2D * H2 = (TH2D *) fs->Get("ECosTheta_Gold");
-    TH1D * h1[10];
-    TH1D * H1[10];
+    TH2D * h2a = (TH2D *) fs->Get("ECosTheta_clas12");
+    TH2D * h2b = (TH2D *) fs->Get("ECosTheta_bonus12");
+
+    TH1D * h1[40];
+    TH1D * h1a[40];
+    TH1D * h1b[40];
 
     double lumi = 1.0e35 * 1.0e-26 * pow(Phys::hbar, 2);
     double time = 3600.0;
 
-    double binsize = 22.0;
+    double binsize = 10.0;
 
     h2->Scale(lumi*time/binsize);
-    H2->Scale(lumi*time/GOLD::NA/binsize);
+    h2a->Scale(lumi*time/binsize);
+    h2b->Scale(lumi*time/binsize);
 
-
-    TH1D * h0 = new TH1D("h0", "", 1, 0.7, 1.7);
+    TH1D * h0 = new TH1D("h0", "", 1, 1.9, 2.9);
     h0->SetStats(0);
-    h0->GetXaxis()->SetTitle("E_{#gamma (c.m.)} (GeV)");
+    h0->GetXaxis()->SetTitle("W (GeV)");
     h0->GetXaxis()->CenterTitle(true);
     h0->GetXaxis()->SetTitleSize(0.06);
     h0->GetXaxis()->SetTitleOffset(1.15);
     h0->GetXaxis()->SetLabelSize(0.055);
-    h0->GetXaxis()->SetRangeUser(0.7, 1.7);
+    h0->GetXaxis()->SetRangeUser(1.9, 2.9);
     h0->GetXaxis()->SetNdivisions(6, 5, 0);
     h0->GetYaxis()->SetTitle("counts / hour / MeV");
     h0->GetYaxis()->CenterTitle(true);
@@ -85,34 +91,93 @@ int main(const int argc, const char * argv[]){
     c0->SetLeftMargin(0.15);
     c0->SetBottomMargin(0.15);
 
-    for (int i = 0; i < 10; i++){
-      h1[i] = h2->ProjectionX(Form("h%d",i), 20 * i + 1, 20 * i + 20);
-      H1[i] = H2->ProjectionX(Form("H%d",i), 20 * i + 1, 20 * i + 20);
-      h1[i]->SetLineColor(4);
-      H1[i]->SetLineColor(2);
-   
-      h0->GetXaxis()->SetRangeUser(0.7, 1.7);
+    TLatex latex;
+    latex.SetTextSize(0.05);
+    latex.SetTextAlign(22);
+    latex.SetTextFont(22);
+    for (int i = 0; i < 40; i++){
+      h1[i] = h2->ProjectionX(Form("hh%d",i), 5 * i + 1, 5 * i + 5);
+      h1[i]->SetLineColor(1);
+      h1a[i] = h2a->ProjectionX(Form("ta%d",i), 5 * i + 1, 5 * i + 5);
+      h1a[i]->SetLineColor(4);
+      h1b[i] = h2b->ProjectionX(Form("tb%d",i), 5 * i + 1, 5 * i + 5);
+      h1b[i]->SetLineColor(2);
+      
+      h0->GetXaxis()->SetRangeUser(1.9, 2.9);
       h0->SetMinimum(0);
-      h0->SetMaximum(max(h1[i]->GetMaximum(), H1[i]->GetMaximum()) * 1.2);
+      h0->SetMaximum(h1[i]->GetMaximum() * 1.2);
 
       c0->Clear();
       h0->DrawClone("axis");
       h1[i]->DrawClone("same");
-      H1[i]->DrawClone("same");
+      h1a[i]->DrawClone("same");
+      h1b[i]->DrawClone("same");
+      latex.DrawLatex(2.4, h1[i]->GetMaximum() * 1.1, Form("%.2f < cos#theta^{#phi}_{c.m.} < %.2f", -1.0 + 0.05 * i, -0.95 + 0.05 * i));
 
       if (i == 0)
 	c0->Print("gallary/bumpdetectedcos.pdf(", "pdf");
-      else if (i == 9)
+      else if (i == 39)
 	c0->Print("gallary/bumpdetectedcos.pdf)", "pdf");
       else
 	c0->Print("gallary/bumpdetectedcos.pdf", "pdf");
     }
-
-
   }
 
 
+  if (opt == 3){//phi acceptance
+    TFile * fs = new TFile("acceptance/acceptance_phi.root", "r");
+    TH2D * h0 = (TH2D *) fs->Get("acceptance_PTheta_all");
+    TH2D * ha = (TH2D *) fs->Get("acceptance_PTheta_clas12");
+    TH2D * hb = (TH2D *) fs->Get("acceptance_PTheta_bonus12");
+    cout << "aaa" << endl;
+    TH2D * hB = new TH2D("hB", "", 1, 0.0, 4.0, 1, 0.0, M_PI);
+    hB->SetStats(0);
+    hB->GetXaxis()->SetTitle("P (GeV)");
+    hB->GetXaxis()->CenterTitle(true);
+    hB->GetXaxis()->SetTitleSize(0.06);
+    hB->GetXaxis()->SetTitleOffset(1.15);
+    hB->GetXaxis()->SetLabelSize(0.055);
+    hB->GetXaxis()->SetRangeUser(0.0, 4.0);
+    hB->GetXaxis()->SetNdivisions(6, 5, 0);
+    hB->GetYaxis()->SetTitle("#theta (rad)");
+    hB->GetYaxis()->CenterTitle(true);
+    hB->GetYaxis()->SetTitleSize(0.06);
+    hB->GetYaxis()->SetTitleOffset(1.15);
+    hB->GetYaxis()->SetLabelSize(0.055);
+    hB->GetYaxis()->SetRangeUser(0.0, M_PI);
+    hB->GetXaxis()->SetNdivisions(6, 5, 0);
+    
+    TLatex latex;
+    latex.SetTextFont(22);
+    latex.SetTextAlign(22);
+    latex.SetTextSize(0.055);
+    TCanvas * c0 = new TCanvas("c0", "", 800, 600);
+    c0->SetLeftMargin(0.15);
+    c0->SetBottomMargin(0.15);
+    hB->DrawClone("axis");
+    h0->DrawClone("colzsame");
+    latex.DrawLatex(2.2, M_PI * 1.05, "CLAS12 + BONUS12");
+    c0->Print("gallary/phiacceptance.pdf(", "pdf");
 
+    c0->Clear();
+    hB->DrawClone("axis");
+    ha->DrawClone("colzsame");
+    latex.DrawLatex(2.2, M_PI * 1.05, "CLAS12 only");
+    c0->Print("gallary/phiacceptance.pdf", "pdf");
+    
+    c0->Clear();
+    hB->DrawClone("axis");
+    hb->DrawClone("colzsame");
+    latex.DrawLatex(2.2, M_PI * 1.05, "BONUS12 only");
+    c0->Print("gallary/phiacceptance.pdf)", "pdf");
+
+    
+
+    
+  }
+
+
+  
   return 0;
 }
 
